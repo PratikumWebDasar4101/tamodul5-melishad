f436632443bbebdbfcde95255c6117aca92947c8
melishad <42725372+melishad@users.noreply.github.com>
2018-10-02 20:14:46 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,30 @@
-<?php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="proseslogin.php" method="post">
+	<center>
+		<h2> Login Terlebih Dahulu</h2><br>
+		<table>
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="text" name="username" placeholder="Username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="password" name="password" placeholder="Password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td><input type="submit" name="kirim" value="SUBMIT"></td>
+			</tr>
+		</table>
+	</center>
+</form>
+</body>
+</html>
